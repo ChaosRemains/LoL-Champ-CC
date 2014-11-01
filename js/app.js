c04f838cce26ad63502542acdfd06f2569eb87d1
ChaosRemains <lordmaster6@gmail.com>
2014-11-01 01:54:24 -0400

    Champs
    Added Champs names and CC

--- js/app.js
+++ js/app.js
@@ -1,15 +1,432 @@
 var champions = {
+	"Aatrox": {
+		Knockups: 1,
+		Slows: 1
+	},
+	"Ahri": {
+		Charms: 1
+	},
+	"Akali": {
+		Slows: 1
+	},
+	"Alistar": {
+		Knockups: 1,
+		Knockbacks: 1
+	},
+	"Amumu": {
+		Stuns: 2
+	},
+	"Anivia": {
+		Stuns: 1,
+		Slows: 2,
+		Terrain: 1
+	},	
 	"Annie": {
-		stuns: 1
+		Stuns: 1
+	},
+	"Ashe": {
+		Slows: 2,
+		Stuns: 1
+	},
+	"Azir": {
+		Slows: 1,
+		Knockbacks: 1
+	},
+	"Blitzcrank": {
+		Pulls: 1,
+		Knockups: 1,
+		Silences: 1
+	},
+	"Brand": {
+		Stuns: 1
+	},
+	"Braum": {
+		Stuns: 1,
+		Slows: 2,
+		Knockups: 1
+	},
+	"Caitlyn": {
+		Slows: 1,
+		Roots: 1
+	},
+	"Cassiopeia": {
+		Slows: 1,
+		Stuns: 1
+	},
+	"Cho'gath": {
+		Slows: 1,
+		Silences: 1,
+		Knockups: 1
+	},
+	"Corki": {},
+	"Darius": {
+		Slows: 1,
+		Pulls: 1
+	},
+	"Diana": {
+		Slows: 1
+	},
+	"Dr.Mundo": {
+		Slows: 1
+	},
+	"Draven": {
+		Slows: 1,
+		Knockbacks: 1
+	},
+	"Elise": {
+		Stuns: 1
+	},
+	"Evelynn": {
+		Slows: 1
+	},
+	"Ezreal": {},
+	"Fiddlesticks": {
+		Flees: 1,
+		Silences: 1
+	},
+	"Fiora": {},
+	"Fizz": {
+		Slows: 2
+	},
+	"Galio": {
+		Slows: 1,
+		Taunts: 1
+	},
+	"Gangplank": {
+		Slows: 2
+	},
+	"Garen": {
+		Silences: 1
+	},
+	"Gnar": {
+		Slows: 3,
+		Stuns: 2
+	},
+	"Gragas": {
+		Slows: 1,
+		Stuns: 1,
+		Knockbacks: 1
+	},
+	"Graves": {
+		Slows: 1
+	},
+	"Hecarim": {
+		Fears: 1
+	},
+	"Heimerdinger": {
+		Stuns: 1,
+		Slows: 1
+	},
+	"Irelia": {
+		Stuns: 1
+	},
+	"Janna": {
+		Knockups: 1,
+		Slows: 1,
+		Knockbacks: 1
+	},
+	"Jarvan IV": {
+		Slows: 1,
+		Knockups: 1,
+		Terrain: 1
+	},
+	"Jax": {
+		Stuns: 1
+	},
+	"Jayce": {
+		Slows: 1,
+		Knockbacks: 1
+	},
+	"Jinx": {
+		Slows: 1,
+		Roots: 1
+	},
+	"Karma": {
+		Slows: 1,
+		Roots: 1
+	},
+	"Karthus": {
+		Slows: 1
+	},
+	"Kassadin": {
+		Slows: 1,
+		Interrupts: 1
+	},
+	"Katarina": {},
+	"Kayle": {
+		Slows: 1
+	},
+	"Kennen": {
+		Stuns: 1
+	},
+	"Kha'zix": {
+		Slows: 2
+	},
+	"Kog'Maw": {
+		Slows: 1
+	},
+	"Leblanc": {
+		Slows: 1,
+		Roots: 1
+	},
+	"Lee Sin": {
+		Slows: 1,
+		Knockbacks: 1
+	},
+	"Leona": {
+		Stuns: 2,
+		Roots: 1
+	},
+	"Lissandra": {
+		Slows: 1,
+		Roots: 1,
+		Stuns: 1
+	},
+	"Lucian": {},
+	"Lulu": {
+		Slows: 2,
+		Polymorph: 1
 	},
 	"Lux": {
-		snares: 1,
-		slows: 1
+		Slows: 1,
+		Roots: 1
+	},
+	"Malphite": {
+		Slows: 1,
+		Knockups: 1
+	},
+	"Malzahar": {
+		Silences: 1,
+		Suppresses: 1
+	},
+	"Maokai": {
+		Slows: 2,
+		Roots: 1
+	},
+	"Master Yi": {},
+	"Miss Fortune": {
+		Slows: 1
+	},
+	"Mordekaiser": {},
+	"Morgana": {
+		Roots: 1,
+		Stuns: 1,
+		Slows: 1
+	},
+	"Nami": {
+		Slows: 2,
+		Knockups: 2
+	},
+	"Nasus": {
+		Slows: 1
+	},
+	"Nautilus": {
+		Roots: 1,
+		Pulls: 1,
+		Slows: 2,
+		Knockups: 1,
+		Stuns: 1
+	},
+	"Nidalee": {},
+	"Nocturne": {
+		Flees: 1
+	},
+	"Nunu": {
+		Slows: 2
+	},
+	"Olaf": {
+		Slows: 1
+	},
+	"Orianna": {
+		Slows: 1,
+		Knockups: 1
+	},
+	"Pantheon": {
+		Stuns: 1
+	},
+	"Poppy": {
+		Stuns: 1
+	},
+	"Quinn": {
+		Slows: 1,
+		Blinds: 1
+	},
+	"Rammus": {
+		Slows: 1,
+		Knockbacks: 1,
+		Taunts: 1
+	},
+	"Renekton": {
+		Stuns: 1
+	},
+	"Rengar": {
+		Slows: 1,
+		Roots: 1
+	},
+	"Riven": {
+		Stuns: 1,
+		Knockups: 1
+	},
+	"Rumble": {
+		Slows: 2
+	},
+	"Ryze": {
+		Roots: 1
+	},
+	"Sejuani": {
+		Slows: 2,
+		Knockups: 1,
+		Stuns: 1
+	},
+	"Shaco": {
+		Slows: 1,
+		Flees: 1
+	},
+	"Shen": {
+		Taunts: 1
+	},
+	"Shyvana": {
+		Knockbacks: 1
+	},
+	"Singed": {
+		Slows: 1,
+		Pulls: 1
+	},
+	"Sion": {
+		Slows: 1,
+		Knockups: 2
+	},
+	"Sivir": {},
+	"Skarner": {
+		Slows: 1,
+		Stuns: 1,
+		Supresses: 1
+	},
+	"Sona": {
+		Slows: 1,
+		Stuns: 1
+	},
+	"Soraka": {
+		Slows: 1,
+		Roots: 1,
+		Silences: 1
+	},
+	"Swain": {
+		Slows: 1,
+		Roots: 1
+	},
+	"Syndra": {
+		Slows: 1,
+		Stuns: 1
+	},
+	"Talon": {
+		Slows: 2
+	},
+	"Taric": {
+		Stuns: 1
+	},
+	"Teemo": {
+		Slows: 1,
+		Blinds: 1
+	},
+	"Thresh": {
+		Slows: 2,
+		Pulls: 1
+	},
+	"Tristana": {
+		Slows: 1,
+		Knockbacks: 1
+	},
+	"Trundle": {
+		Slows: 1,
+		Terrain: 1
+	},
+	"Tryndamere": {
+		Slows: 1
+	},
+	"Twisted Fate": {
+		Slows: 1,
+		Stuns: 1
+	},
+	"Twitch": {
+		Slows: 1
+	},
+	"Udyr": {
+		Stuns: 1
+	},
+	"Urgot": {
+		Slows: 2,
+		Suppresses: 1
 	},
 	"Varus": {
-		slows: 1,
-		stuns: 1	
-	}
+		Slows: 1,
+		Roots: 1
+	},
+	"Vayne": {
+		Stuns: 1
+	},
+	"Veigar": {
+		Stuns: 1
+	},
+	"Vel'Koz": {
+		Slows: 1,
+		Knockups: 1
+	},
+	"Vi": {
+		Knockbacks: 1,
+		Knockups: 1
+	},
+	"Viktor": {
+		Stuns: 1
+	},
+	"Vladimir": {
+		Slows: 1
+	},
+	"Volibear": {
+		Slows: 1,
+		Pulls: 1
+	},
+	"Warwick": {
+		Suppresses: 1
+	},
+	"Wukong": {
+		Knockups: 1
+	},
+	"Xerath": {
+		Slows: 1,
+		Stuns: 1
+	},
+	"Xin Zhao": {
+		Slows: 1,
+		Knockbacks: 1,
+		Knockups: 1
+	},
+	"Yasuo": {
+		Knockups: 1
+	},
+	"Yorick": {
+		Slows: 1
+	},
+	"Zac": {
+		Slows: 2,
+		Knockbacks: 1,
+		Knockups: 1
+	},
+	"Zed": {
+		Slows: 1
+	},
+	"Ziggs": {
+		Slows: 1,
+		Knockbacks: 1
+	},
+	"Zilean": {
+		Slows: 1
+	},
+	"Zyra": {
+		Slows: 1,
+		Roots: 1,
+		Knockups: 1
+	},
 };
 
 console.log(champions);
